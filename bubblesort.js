ef2415bd05bfcf35e87abcd52e3b9d40eb7f55d8
Norka Avalos <norka.avalos11@gmail.com>
2018-11-05 12:32:50 -0500

    implementing bubbleSort

--- bubblesort.js
+++ bubblesort.js
@@ -1,12 +1,17 @@
 function swap(num1, num2) {
-  if (num1 > num2) {
     let temp = num1;
     num1 = num2;
     num2 = temp;
     return [num1, num2];
-  } else {
-    return [num1, num2];
-  }
+
 }
 
-function bubbleSort(array) {}
+function bubbleSort(array) {
+  for (let i=0; i<array.length -1; i++){
+    for (let j= i+1; j< array.length; j++){
+      if (array[i] > array[j]){
+        swap(array[i],array[j])
+      }
+    }
+  }
+}
